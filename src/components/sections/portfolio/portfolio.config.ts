fb07de09b9c4dca4970dd9d39aa4ea6304ec82db
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-07-23 01:35:19 +0300

    feat: add portfolio

--- src/components/sections/portfolio/portfolio.config.ts
+++ src/components/sections/portfolio/portfolio.config.ts
@@ -25,6 +25,22 @@ export const portfolioConfig: PortfolioItem[] = [
     demo: 'https://reactful-swagger-editor-app.vercel.app/',
   },
   {
+    title: 'RS School React App',
+    description:
+      'This is a single-page search application that works with a public REST API supporting search and pagination.',
+    badges: [
+      'Next.js',
+      'React',
+      'Typescript',
+      'Redux Toolkit',
+      'RTK Query',
+      'next-intl',
+      'Vitest',
+    ],
+    github: 'https://github.com/rika-milew/rs-react-app',
+    demo: 'https://rika-milew-rs-react-app.vercel.app/',
+  },
+  {
     title: 'Tandem App',
     description:
       'Web application for preparing for technical interviews with interactive widgets.',
@@ -40,6 +56,15 @@ export const portfolioConfig: PortfolioItem[] = [
     github:
       'https://github.com/rolling-scopes-school/rika-milew-JSFE2025Q3/tree/rss-puzzle/rss-puzzle',
     demo: 'https://rolling-scopes-school.github.io/rika-milew-JSFE2025Q3/rss-puzzle/',
+  },
+  {
+    title: 'Async Race',
+    description:
+      'A single-page application for managing a car collection and running dracing competitions.',
+    badges: ['TypeScript', 'Vite', 'REST API', 'CSS Animations', 'SPA'],
+    github:
+      'https://github.com/rika-milew/rika-milew-JSFE2025Q3/tree/async-race',
+    demo: 'https://rolling-scopes-school.github.io/rika-milew-JSFE2025Q3/async-race/',
   },
   {
     title: 'NFT Landing Page',
