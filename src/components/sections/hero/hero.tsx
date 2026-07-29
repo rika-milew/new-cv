a6526a5f59b31b9b1d0bfafafafb852d669efd73
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-07-30 02:00:22 +0300

    feat: add anchors

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link';
 import classNames from 'classnames/bind';
 import Image from 'next/image';
+import { ANCHORS } from '@/constants';
 import { Button } from '@/components/button/button';
 import { Heading } from '@/components/heading/heading';
 import { Planet } from '@/components/animation/planet/planet';
+import { useSmoothScroll } from '@/hooks/use-smooth-scroll';
 import styles from './hero.module.css';
 
 const cx = classNames.bind(styles);
 
 export function Hero() {
+  const scrollTo = useSmoothScroll();
+
   return (
     <section id="hero" className={cx('section', 'hero')}>
       <Planet />
@@ -23,18 +27,27 @@ export function Hero() {
           cosmic magic ✨
         </p>
         <div className={cx('buttons-container')}>
-          <Button href="/#about" variant="primary">
+          <Button
+            href={ANCHORS.ABOUT}
+            variant="primary"
+            onClick={(e) => scrollTo(e, ANCHORS.ABOUT)}
+          >
             About Me
           </Button>
-          <Button href="/#contacts" variant="secondary">
+          <Button
+            href={ANCHORS.CONTACTS}
+            variant="secondary"
+            onClick={(e) => scrollTo(e, ANCHORS.CONTACTS)}
+          >
             Contact Me
           </Button>
         </div>
       </div>
       <Link
-        href="/#about"
+        href={ANCHORS.ABOUT}
         className={cx('arrow-icon')}
         aria-label="Scroll to about"
+        onClick={(e) => scrollTo(e, ANCHORS.ABOUT)}
       >
         <Image src="/icons/arrow.svg" alt="" width={28} height={28} />
       </Link>
